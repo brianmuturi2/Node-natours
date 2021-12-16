0536f6eed96e070dfa64dbe40988efbd03b76065
brianmuturi2 <brianmuturi2@gmail.com>
2021-12-16 18:28:13 +0300

    Natours: started on get tours api

---
+++ app.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+/*app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from the server side!', app: 'natours' });
+});
+app.post('/', (req, res) => {
+  res.status(200).send('You can post to this endpoint')
+})*/
+
+// Get tours to serve
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+// Get requests
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      }
+    })
+})
+
+
+// Start server
+const port = 3000;
+
+app.listen(port,  () => {
+  console.log(`App running on port ${port}`);
+});
+
