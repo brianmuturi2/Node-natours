5016c856aaaeb7b4e5d0578e7160384a2a65a90d
brianmuturi2 <brianmuturi2@gmail.com>
2022-01-05 18:03:02 +0300

    Natours: handled unhandled rejections, errors outside express

--- server.js
+++ server.js
@@ -16,8 +16,18 @@ mongoose.connect(DB, {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+process.on('unhandledRejection', (err) => {
+  // shut down application; e.g if app cant connect to db, it wont work thus shutting down is the best solution
+  // the problem with process.exit is an abrupt way of ending a program, aborting all running or pending requests
+  // you can shut down gracefully, close the server, close the application
+  // you can have a tool to restart the app or the platform hosting the app can restart it automatically
+  server.close(() => {
+    process.exit(1); // code 0 is success; code 1 is uncalled exception
+  });
+})
 
 module.exports = app;
