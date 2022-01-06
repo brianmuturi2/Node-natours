e1b92cff611a1ea3b3962f9f03bb7c6b96ca8c67
brianmuturi2 <brianmuturi2@gmail.com>
2022-01-06 19:37:41 +0300

    Natours: implemented password management; check confirm password and password are same; encrypted password; removed confirm password from database

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,9 +24,29 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on create and save (cant use findOneAndUpdate mongoose method)
+      validator: function(confirmPassword) {
+        return confirmPassword === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
   }
 });
+
+userSchema.pre('save', async function(next) {
+  // only run if password is modified
+  if (!this.isModified('password')) return next();
+
+  // hash password with cost of 12
+  // 12 is the computational cost for encrypting; the higher the stronger but slower
+  this.password = await bcrypt.hash(this.password, 11);
+
+  // delete confirm password field
+  this.passwordConfirm = undefined;
+  next();
+})
 
 const User = mongoose.model('User', userSchema);
 
