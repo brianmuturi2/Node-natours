0c94798f8ebde105d65de69a3cb7d842eeb5825d
brianmuturi2 <brianmuturi2@gmail.com>
2022-01-06 18:34:36 +0300

    Natours: created users schema & model

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true, // transform email to lowercase
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: {
+    type: String, // url to image in file system
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
