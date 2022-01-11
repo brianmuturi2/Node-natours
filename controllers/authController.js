32c7b2de98eb657e1bd37dbb1e98b8b41d394794
brianmuturi2 <brianmuturi2@gmail.com>
2022-01-11 20:03:47 +0300

    Natours: started on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/AppError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -21,4 +22,24 @@ exports.signup = catchAsync(async (req, res, next) => {
       user: newUser
     }
   })
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return  next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2) Check if user exists and password is correct
+  const user = await User.findOne({email}).select('+password')
+  console.log(user);
+
+  // 3) If everything is  ok, send token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token
+  });
 });
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,25 @@
+const User = require('../models/userModel');
+const APIFeatures = require('../utils/ApiFeatures');
+const catchAsync = require('../utils/catchAsync');
+
 // User(s) route handlers TODO
 
-exports.getAllUsers = (req, res) => {
+exports.getAllUsers = catchAsync (async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
+  });
+
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined'
   });
-}
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
