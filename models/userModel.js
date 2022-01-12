ba5e2da219f70a329fbd29244627efdc5f51ad3b
brianmuturi2 <brianmuturi2@gmail.com>
2022-01-12 18:51:49 +0300

    Natours: implemented login

--- models/userModel.js
+++ models/userModel.js
@@ -47,7 +47,11 @@ userSchema.pre('save', async function(next) {
   // delete confirm password field
   this.passwordConfirm = undefined;
   next();
-})
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 
