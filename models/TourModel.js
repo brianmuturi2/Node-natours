bc1ab0dc1a4483dfaee6f30f5cd7401e88e42c0a
brianmuturi2 <brianmuturi2@gmail.com>
2022-02-21 17:34:02 +0300

    feat(tours): implemented embedding users/guides documents in tours document

--- models/TourModel.js
+++ models/TourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -103,7 +104,8 @@ const tourSchema = new mongoose.Schema({
       description: String,
       day: Number
     },
-  ]
+  ],
+  guides: Array
 }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
@@ -130,6 +132,12 @@ tourSchema.pre(/^find/, function(next) {
 });
 
 tourSchema.post(/^find/, function(docs, next) {
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async  id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises)
   next();
 });
 
