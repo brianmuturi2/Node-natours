41a39af868600bf89a3a36a2894a986fdf6b4a23
brianmuturi2 <brianmuturi2@gmail.com>
2022-02-24 19:59:32 +0300

    feat(tours): started on calculating reviews statistics

--- models/ReviewModel.js
+++ models/ReviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./TourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -44,6 +45,31 @@ reviewSchema.pre(/^find/, function(next) {
     select: 'name photo'
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+   const stats = await this.aggregate([
+     {
+       $match: {tour: tourId}
+     },
+     {
+       $group: {
+         _id: '$tour',
+         nRating: { $sum: 1},
+         avgRating: { $avg: '$rating'}
+       }
+     }
+   ]);
+   console.log('tour stats: ', stats);
+   await Tour.findByIdAndUpdate(tourId, {
+     ratingsQuantity: stats[0]['nRating'],
+     ratingsAverage: stats[0]['avgRating']
+   })
+}
+
+reviewSchema.post('save', function() {
+  // this points to current document being saved i.e current review
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
