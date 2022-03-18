1d5cba7dbbbbf208db1f122b714431cb62a07d64
brianmuturi2 <brianmuturi2@gmail.com>
2022-03-18 08:44:42 +0300

    feat(login): started on the login view

--- views/viewRoutes.js
+++ views/viewRoutes.js
@@ -3,8 +3,10 @@ const viewsController = require('../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/ ', viewsController.getOverview);
+router.get('/', viewsController.getOverview);
 
 router.get('/tour/:slug', viewsController.getTour);
+
+router.get('/login', viewsController.getLoginForm);
 
 module.exports = router;
