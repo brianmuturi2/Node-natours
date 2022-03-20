f141c1263d0bc9561940d659205231dad06a9bea
brianmuturi2 <brianmuturi2@gmail.com>
2022-03-20 13:33:22 +0300

    feat(map): finished on map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,10 +1,40 @@
-console.log('hello from the client side :D');
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+export const displayMap = locations => {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpYW5tdXR1cmkyIiwiYSI6ImNsMHV6Yjd6ejBpdm0zaWtiOHlpZDJvdjcifQ.k1ZdrX4Rrh9hImeWJnWO2w';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpYW5tdXR1cmkyIiwiYSI6ImNsMHV6Yjd6ejBpdm0zaWtiOHlpZDJvdjcifQ.k1ZdrX4Rrh9hImeWJnWO2w';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/brianmuturi2/cl0z2sjkw00u814qylkw3ic54',
+    scrollZoom: false
+    //center: [-118.113491, 34.111745],
+    //zoom: 4,
+    //interctive: false
+  });
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
-});
+  const bounds = new mapboxgl.LngLatBounds();
+  locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    }).setLngLat(loc.coordinates).addTo(map);
+
+    // Add popup
+    new mapboxgl.Popup({offset: 30}).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`).addTo(map);
+
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
+
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+}
