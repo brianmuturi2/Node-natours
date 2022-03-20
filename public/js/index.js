5af0d920f48e8a253daca0028924a440648fada8
brianmuturi2 <brianmuturi2@gmail.com>
2022-03-20 21:22:49 +0300

    feat(user account): added update password from user account view

--- public/js/index.js
+++ public/js/index.js
@@ -10,8 +10,11 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const passwordCurrent = document.getElementById('password-current');
+const passwordConfirm = document.getElementById('password-confirm');
 const name = document.getElementById('name');
 const updateUserForm = document.querySelector('.form-user-data');
+const updatePasswordForm = document.querySelector('.form-user-settings');
 
 // DELEGATION
 
@@ -34,6 +37,23 @@ if (logOutBtn) {
 if (updateUserForm) {
   updateUserForm.addEventListener('submit', e => {
     e.preventDefault();
-    updateData(name.value, email.value);
+    updateData({name: name.value, email: email.value}, 'data');
+  });
+}
+
+if (updatePasswordForm) {
+  updatePasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    await updateData({
+      password: password.value,
+      passwordCurrent: passwordCurrent.value,
+      passwordConfirm: passwordConfirm.value
+    }, 'password');
+
+    document.querySelector('.btn--save-password').textContent = 'SAVE PASSWORD';
+    password.value = '';
+    passwordCurrent.value = '';
+    passwordConfirm.value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,18 +2,16 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+// type parameter is either password or data
+export const updateData = async (data, type) => {
   try {
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email
-      }
+      url: type === 'data' ? 'http://127.0.0.1:3000/api/v1/users/updateMe' : 'http://127.0.0.1:3000/api/v1/users/updatePassword',
+      data
     });
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
       location.assign('/me');
     }
   } catch (e) {
