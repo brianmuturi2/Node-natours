e7b2f5350e57576cbb8e8aa5816659a631910ada
brianmuturi2 <brianmuturi2@gmail.com>
2022-03-23 14:43:30 +0300

    feat(bookings): reused overview view for user bookings

--- views/viewRoutes.js
+++ views/viewRoutes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 
 router.get('/me', authController.protect, viewsController.getAccount);
 
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
+
 router.post('/submit-user-data', authController.protect, viewsController.updateUserData)
 
 router.use(authController.isLoggedIn);
