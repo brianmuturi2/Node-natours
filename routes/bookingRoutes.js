46862abaf2eb538c9729a56062603916bb4c4320
brianmuturi2 <brianmuturi2@gmail.com>
2022-03-23 14:58:35 +0300

    feat(bookings): created crud handlers

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,9 +2,21 @@ const express = require('express');
 const bookingController = require('./../controllers/bookingController');
 const authController = require('./../controllers/authController');
 
+router.use(authController.protect);
+
 const router = express.Router();
 
-router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession);
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router.route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router.route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
