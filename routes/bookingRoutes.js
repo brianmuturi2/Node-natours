88686ea91a3dd1f8ac7e5213e36c9aaed0935ecd
brianmuturi2 <brianmuturi2@gmail.com>
2022-04-19 13:13:27 +0300

    fix(booking routes): updated protect middleware

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bookingController = require('./../controllers/bookingController');
 const authController = require('./../controllers/authController');
 
-router.use(authController.protect);
-
 const router = express.Router();
+
+router.use(authController.protect);
 
 router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
